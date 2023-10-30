ca10bb739acbc9fe45c8d9b5da78ec11701a3a44
Osman Demiröz <osmanndemirozz@gmail.com>
2023-10-30 14:48:56 +0300

    added pubdev dependecies

---
+++ lib/features/home/view/mixin/home_view_mixin.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_architecture_template/features/home/view/home_view.dart';
+
+mixin HomeViewMixin on State<HomeView> {
+  @override
+  void initState() {
+    super.initState();
+  }
+}
